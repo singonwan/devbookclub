4b1d737aafe4d2e2b6df5349795ad5014bab11e2
George Wan <singonwan@gmail.com>
2019-10-27 20:19:28 -0400

    user registration - checked whether user exists, got users gravater, created a new user instance and saved it and also hashed the password with bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register User
@@ -17,14 +21,55 @@ router.post(
             'Please enter a password with 6 or more characters'
         ).isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         // console.log(req.body);   { name: 'RH', email: 'rh@gwan.com', password: '123456' }
         // check errors from the checks
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.send('User route');
+
+        const { name, email, password } = req.body;
+
+        try {
+            //check if user exists
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(400).json({
+                    errors: [{ msg: 'User already exists' }] //making it similar to validationresults errors res
+                });
+            }
+            //get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            //create new instance of user
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            //encrypt password using bcrypt - passwords are hashed to protect against attackers who gain read-only access to db
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            //save user     - update document with mongoose function save()
+            await user.save();
+
+            //return jwt
+
+            res.send('User registered');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
     }
 );
 
