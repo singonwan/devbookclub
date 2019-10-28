4bbc94c43653c628194f54df3dfd0387628e4640
George Wan <singonwan@gmail.com>
2019-10-28 01:47:52 -0400

    created route to delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,4 +154,23 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route   DELETE api/profile/
+// @desc    delete profile, user and posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove user's posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User Deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
