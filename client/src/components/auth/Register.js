343c48ae74788aba6220d684874bb432587f95d7
George Wan <singonwan@gmail.com>
2019-11-07 18:24:20 -0500

    successfully completed alerts, used it when passwords dont match in register to call alert using redux. finished setalert and removealert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
+
 import axios from 'axios';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -18,7 +22,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault(); //prevent default behaviour of refreshing the page after submit
         if (password !== password2) {
-            console.log('passwords do not match');
+            setAlert('Passwords do not match', 'danger');
         } else {
             const newUser = {
                 name,
@@ -107,4 +111,12 @@ const Register = () => {
     );
 };
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+//connect(mapStateToProps, mapDispatchToProps)
+export default connect(
+    null,
+    { setAlert }
+)(Register);
