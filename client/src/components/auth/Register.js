e32bcce1054b3ec35808c45d0cf0de9aa15cf342
George Wan <singonwan@gmail.com>
2019-11-07 01:24:55 -0500

    filled out login form as well and also tested register form with axios for backend. will rewrite with redux instead later on

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -13,12 +15,31 @@ const Register = () => {
     const onChange = e =>
         setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault(); //prevent default behaviour of refreshing the page after submit
         if (password !== password2) {
             console.log('passwords do not match');
         } else {
-            console.log(formData);
+            const newUser = {
+                name,
+                email,
+                password
+            };
+
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+
+                const body = JSON.stringify(newUser);
+
+                const res = await axios.post('/api/users', body, config);
+                console.log(res.data);
+            } catch (err) {
+                console.error(err.response.data);
+            }
         }
     };
 
@@ -80,7 +101,7 @@ const Register = () => {
                 />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </Fragment>
     );
