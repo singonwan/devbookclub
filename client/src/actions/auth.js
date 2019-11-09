eadbd67f8e54ce0e74da9aa621a56346c2485443
George Wan <singonwan@gmail.com>
2019-11-08 23:41:55 -0500

    created logout action and customized navbar links for authorized users and guests

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_FAIL,
-    LOGIN_SUCCESS
+    LOGIN_SUCCESS,
+    LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -93,4 +94,11 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_FAIL
         });
     }
+};
+
+// Logout / Clear profile
+export const logout = () => dispatch => {
+    dispatch({
+        type: LOGOUT
+    });
 };
