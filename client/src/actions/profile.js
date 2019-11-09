2f68050f765792c4ff4a6e1886433ece9a9b3c8e
George Wan <singonwan@gmail.com>
2019-11-09 16:09:43 -0500

    completed createprofile action and checked it worked in the backend with mongo, implemented itinto create profile form

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -22,3 +22,50 @@ export const getCurrentProfile = () => async dispatch => {
         });
     }
 };
+
+// Create or Update profile
+export const createProfile = (
+    formData,
+    history,
+    edit = false
+) => async dispatch => {
+    //history object to push and redirect after creating profile
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.post('/api/profile', formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data //route returns current user's profile
+        });
+
+        dispatch(
+            setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+        );
+
+        //redirect only when creating profile
+        if (!edit) {
+            //redirecting in an action, cant use <Redirect/>... have to use history
+            history.push('/dashboard');
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        });
+    }
+};
