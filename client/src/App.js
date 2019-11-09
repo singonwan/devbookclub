f18de5364a1eac925f8162e7f0c2c8fc7b4368b5
George Wan <singonwan@gmail.com>
2019-11-09 13:36:37 -0500

    made the create-profile component form to fill out with useState hook

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -48,6 +49,11 @@ const App = () => {
                                 exact
                                 path="/dashboard"
                                 component={Dashboard}
+                            />
+                            <PrivateRoute
+                                exact
+                                path="/create-profile"
+                                component={CreateProfile}
                             />
                         </Switch>
                     </section>
