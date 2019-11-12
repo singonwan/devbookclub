ece80c05b5c3720777dda7dadecbeea1c0aa56b4
George Wan <singonwan@gmail.com>
2019-11-12 02:33:14 -0500

    created action and reducer for getting all profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,7 @@ import { setAlert } from './alert';
 
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
     UPDATE_PROFILE,
     CLEAR_PROFILE,
@@ -16,6 +17,27 @@ export const getCurrentProfile = () => async dispatch => {
 
         dispatch({
             type: GET_PROFILE,
+            payload: res.data //route returns current user's profile
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        });
+    }
+};
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE }); //prevent flashing of past users profile
+    try {
+        const res = await axios.get('/api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
             payload: res.data //route returns current user's profile
         });
     } catch (err) {
