e9182324dbd1f79685e72e22531ccc83f3a93300
George Wan <singonwan@gmail.com>
2019-11-13 22:12:10 -0500

    created profileeducation and profileexperience to display education and experience on profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -36,6 +38,36 @@ const Profile = ({
                     <div className="profile-grid my-1">
                         <ProfileTop profile={profile} />
                         <ProfileAbout profile={profile} />
+                        <div className="profile-exp bg-white p-2">
+                            <h2 className="text-primary">Experience</h2>
+                            {profile.experience.length > 0 ? (
+                                <Fragment>
+                                    {profile.experience.map(exp => (
+                                        <ProfileExperience
+                                            key={exp._id}
+                                            experience={exp}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>No Experience yet...</h4>
+                            )}
+                        </div>
+                        <div className="profile-edu bg-white p-2">
+                            <h2 className="text-primary">Education</h2>
+                            {profile.education.length > 0 ? (
+                                <Fragment>
+                                    {profile.education.map(edu => (
+                                        <ProfileEducation
+                                            key={edu._id}
+                                            education={edu}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>No Education yet...</h4>
+                            )}
+                        </div>
                     </div>
                 </Fragment>
             )}
