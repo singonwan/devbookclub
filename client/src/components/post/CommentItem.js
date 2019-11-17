b8d1b31074d59e765ad90ceabecddf8705b223a0
George Wan <singonwan@gmail.com>
2019-11-16 19:26:19 -0500

    delete comment functionality added

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,25 +3,36 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post';
 
 const CommentItem = ({
     postId,
     comment: { _id, text, name, avatar, user, date },
-    auth
+    auth,
+    deleteComment
 }) => {
     return (
-        <div class="post bg-white p-1 my-1">
+        <div className="post bg-white p-1 my-1">
             <div>
                 <Link to={`/profile/${user}`}>
-                    <img class="round-img" src={avatar} alt="" />
+                    <img className="round-img" src={avatar} alt="" />
                     <h4>{name}</h4>
                 </Link>
             </div>
             <div>
-                <p class="my-1">{text}</p>
-                <p class="post-date">
+                <p className="my-1">{text}</p>
+                <p className="post-date">
                     Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
                 </p>
+                {!auth.loading && user === auth.user._id && (
+                    <button
+                        onClick={e => deleteComment(postId, _id)}
+                        type="button"
+                        className="btn btn-danger"
+                    >
+                        <i className="fas fa-times"></i>
+                    </button>
+                )}
             </div>
         </div>
     );
@@ -30,7 +41,8 @@ const CommentItem = ({
 CommentItem.propTypes = {
     postId: PropTypes.number.isRequired,
     comment: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    deleteComment: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -39,5 +51,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    {}
+    { deleteComment }
 )(CommentItem);
